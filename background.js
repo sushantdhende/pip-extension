a059fa730ed93acc64c43987ba4f176124e21bab
sushant dhende <sdhende1@gmail.com>
2024-08-04 17:30:14 +0530

    First commit

---
+++ background.js
@@ -0,0 +1,37 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true },
+    function: togglePictureInPicture
+  });
+});
+
+function togglePictureInPicture() {
+  const video = document.querySelector('video');
+  if (video) {
+    if (document.pictureInPictureElement) {
+      document.exitPictureInPicture();
+    } else {
+      video.requestPictureInPicture();
+    }
+  } else {
+    // If no video is found in the main document, check iframes
+    const iframes = document.querySelectorAll('iframe');
+    for (const iframe of iframes) {
+      try {
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        const iframeVideo = iframeDocument.querySelector('video');
+        if (iframeVideo) {
+          if (iframeDocument.pictureInPictureElement) {
+            iframeDocument.exitPictureInPicture();
+          } else {
+            iframeVideo.requestPictureInPicture();
+          }
+          return;
+        }
+      } catch (e) {
+        console.error('Cannot access iframe:', e);
+      }
+    }
+    alert('No video element found on this page.');
+  }
+}
